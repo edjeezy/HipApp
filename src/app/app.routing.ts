09922fdf10ab63689e38600451e8655b7dd189cc
edjeezy <mouhamededji@gmail.com>
2017-01-19 15:35:02 +0000

    updated routes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
 import { MusicComponent } from './music/music.component';
+import { BlogComponent } from './blog/blog.component';
+import { MarketComponent } from './market/market.component';
 
 const routes: Routes = [
     { path: '', redirectTo : 'music', pathMatch : 'full' },
+    { path: 'home', component : HomeComponent },
+    { path: 'market', component : MarketComponent },
+    { path: 'blog', component: BlogComponent },
     { path: 'music', component: MusicComponent },
 ];
 
